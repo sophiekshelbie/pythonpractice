557823459d429255241ac898f8f19561020461b2
mariam sophie <sophiemariam2@gmail.com>
2019-09-17 15:10:08 +0300

    initial commit

---
+++ exercise1.py
@@ -0,0 +1,5 @@
+Name = input("Please enter your name: ")
+Age = int(input("Please enter your age: "))
+
+year= str((2014 - Age)+100)
+print(Name + " will be 100 years old in the year " + year)
---
+++ exercise9.py
@@ -0,0 +1,26 @@
+# Keep the game going until the user types “exit”
+# Keep track of how many guesses the user has taken, and when the game ends, print this out.
+
+import random
+
+number = random.randint(1,9)
+guess = 0
+count = 0
+
+
+while guess != number and guess != "exit":
+    guess = input("What's your guess?")
+    
+    if guess == "exit":
+        break
+    
+    guess = int(guess)
+    count += 1
+    
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+    else:
+        print("You got it!")
+        print("And it only took you",count,"tries!")
